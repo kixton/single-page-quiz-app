1fde9a6ade53453751357a217969fd34d3e1fabb
Kim Pham <ms.kimpham@gmail.com>
2014-10-11 07:46:17 -0700

    quiz navbar; onClick of quiz name, quiz begins; onClick of answer + submit, show for 1 sec if (in)correct then proceeds to next question

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,22 +1,117 @@
 $(document).ready(function() {
 
   $.get( "/quizzes", function(data) {
-    console.log(data);
     var quizzes = data;
     _.each(quizzes, function(quiz) {
       
-      var title = quiz.title;
-      var id = quiz.id;
-      var quiz_path = "/quizzes/" + id;
-      
-      console.log( quiz.title );
-      console.log( quiz_path );
-
-      $("<a>").attr("href", quiz_path)
-
-      // render quiz template here with quiz info in the quiz var
+      var anchor_tag = $("<a></a>").attr("href", "#").attr("quiz-id", quiz.id).html(quiz.title);
+      $("<li></li>").append(anchor_tag).appendTo(".all-quizzes");
     });
+  });
 
-  })
+  
+  var i = 0; // question counter
+  var j = 0; // answer counter
+  var answersMCTemplate = "<input type='radio' name='answer-choice' class='answer-choice'>";
+  var answersTFTemplate;
+  var formBody = "<form class='quiz-answers'></form>";
+  var selectedAnswer;
+  var quizId;
+
+  var displayQuestion = function(data) {
+
+    // clear the div then create a form
+    $(".main-body").empty();
+    $(".main-body").html(data[i].question).append();
+    $(formBody).appendTo(".main-body");
+
+    // sanitize answers into array
+    var answerChoices = data[i].choices.split(";");
+
+    // loop through answers and display as radio buttons
+    _.each(answerChoices, function(answer) {
+      var x = $(".main-body").find(".quiz-answers");
+      x.append(answersMCTemplate);
+      x.append(answer + "<br>");
+      var y = $(".main-body").find("input").last().attr("value", answer);
+      j++; // increment answer counter
+
+      if (j == answerChoices.length) {
+        x.append("<input type='submit' value='SUBMIT' class='submit'>");
+        j = 0; // reset answer counter
+      }
+    }); // _.each 
+
+    // send data along with submitAnswer function
+    submitAnswer(data);
+
+  };
+
+  var submitAnswer = function(data) {
+
+    // get selectedAnswer
+    $("input:radio[name=answer-choice]").click(function() {
+      selectedAnswer = $(this).val();
+      console.log(selectedAnswer);
+    }); // input.click
+
+    // compare to correctAnswer
+    $(".main-body").find("form").submit(function(e) {
+      e.preventDefault();
+      var correctAnswer = data[i].answer;
+
+      // GET /quizzes/:id/questions/:id/check?answer=your_answer_here
+
+      var questionId = data[i].id;
+      // console.log("question ID " + questionId);
+
+      $.get("/quizzes/" + quizId + "/questions/" + questionId + "check?answer=" + selectedAnswer, function(data) {
+
+      }); 
+
+      if ( selectedAnswer === correctAnswer) {
+        $(".main-body").append("<span class='correct'>Correct!</span>");
+        // increment question count
+        i++;
+        // display nextQuestion
+        nextQuestion(data);
+      } else {
+        $(".main-body").append("<span class='incorrect'>Sorry, that is incorrect.</span>");
+        i++;
+        nextQuestion(data);
+      }
+    }); // form.submit
+  };
+
+  var nextQuestion = function(data) {
+
+    if (i < data.length) {
+      setTimeout(function() {
+        displayQuestion(data);
+        console.log("next question displayed");
+      }, 1000);
+    // if this is the last question in the quiz...
+    } else if (i === data.length) {
+      setTimeout(function() {
+        $(".main-body").empty();
+        $(".main-body").html("You completed the quiz!");
+        i = 0; // reset question counter
+      }, 1000);
+    }
+
+  };
+
+  $(".all-quizzes").on("click", "a", function() {
+
+    quizId = $(this).attr("quiz-id");
+    console.log("quiz ID " + quizId);
+
+    $.get("/quizzes/" + quizId + "/questions", function(data) {
+      displayQuestion(data);
+    }); // .get
+  }); // on("click", "a")
+
+
 
 });
+
