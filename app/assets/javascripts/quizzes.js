a9740d4279f9f388eee9dc60df36057ff2474d9d
Kim Pham <ms.kimpham@gmail.com>
2014-10-13 16:04:53 -0700

    create quiz

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,45 +1,50 @@
 $(document).ready(function() {
 
+  $(".about-project").show();
+
   $.get( "/quizzes", function(data) {
     var quizzes = data;
-    _.each(quizzes, function(quiz) {
-      
+    _.each(quizzes, function(quiz) {     
       var anchor_tag = $("<a></a>").attr("href", "#").attr("quiz-id", quiz.id).html(quiz.title);
       $("<li></li>").append(anchor_tag).appendTo(".all-quizzes");
     });
   });
 
-  
+  var user_id = 1;
   var i = 0; // question counter
   var j = 0; // answer counter
+  var k = 0; // correct answers counter
   var answersMCTemplate = "<input type='radio' name='answer-choice' class='answer-choice'>";
   var answersTFTemplate;
   var formBody = "<form class='quiz-answers'></form>";
   var selectedAnswer;
   var quizId;
+  var submitButton = "<input type='submit' value='SUBMIT' class='submit'>";
 
   var displayQuestion = function(data) {
 
     // clear the div then create a form
-    $(".main-body").empty();
+    $(".about-project").hide();
+    $(".quiz-info").empty();
+
     var question_span = "<span class='question'></span>";
-    $(".main-body").append(question_span);
+    $(".quiz-info").append(question_span);
     $(".question").html(data[i].question).append();
-    $(formBody).appendTo(".main-body");
+    $(formBody).appendTo(".quiz-info");
 
     // sanitize answers into array
     var answerChoices = data[i].choices.split(";");
 
     // loop through answers and display as radio buttons
     _.each(answerChoices, function(answer) {
-      var x = $(".main-body").find(".quiz-answers");
+      var x = $(".quiz-info").find(".quiz-answers");
       x.append(answersMCTemplate);
       x.append(answer + "<br>");
-      var y = $(".main-body").find("input").last().attr("value", answer);
+      var y = $(".quiz-info").find("input").last().attr("value", answer);
       j++; // increment answer counter
 
       if (j == answerChoices.length) {
-        x.append("<input type='submit' value='SUBMIT' class='submit'>");
+        x.append(submitButton);
         j = 0; // reset answer counter
       }
     }); // _.each 
@@ -54,40 +59,30 @@ $(document).ready(function() {
     // get selectedAnswer
     $("input:radio[name=answer-choice]").click(function() {
       selectedAnswer = $(this).val();
-      // console.log("selected answer: " + selectedAnswer);
     }); // input.click
 
-    // compare to correctAnswer
     $(".main-body").find("form").submit(function(e) {
       e.preventDefault();
       var correctAnswer = data[i].answer;
 
-      // GET /quizzes/:id/questions/:id/check?answer=your_answer_here
-
       var questionId = data[i].id;
-      // console.log("question ID " + questionId);
 
-      $.get("/quizzes/" + quizId + "/questions/" + questionId + "check?answer=" + selectedAnswer, function(data) {
-        console.log("getting answer");
-        console.log(data);
+      // check correct answer for question
+      var questionScorePath = "/quizzes/" + quizId + "/questions/" + questionId + "/check?answer=" + selectedAnswer;
 
-        $.get("/quizzes/" + quizId + "/scores", function(data) {
-          console.log(data);
-        });
+      $.get(questionScorePath, function(data) {
 
-      }); 
+        if (data.correct === true) {
+          $(".quiz-info").append("<span class='correct'>Correct!</span>");
+          k++; // increment question count
+        } else if (data.correct === false) {
+          $(".quiz-info").append("<span class='incorrect'>Sorry, that is incorrect.</span>");
+        }
+      }); // get questionScore 
 
-      // if ( selectedAnswer === correctAnswer) {
-      //   $(".main-body").append("<span class='correct'>Correct!</span>");
-      //   // increment question count
-      //   i++;
-      //   // display nextQuestion
-      //   nextQuestion(data);
-      // } else {
-      //   $(".main-body").append("<span class='incorrect'>Sorry, that is incorrect.</span>");
-      //   i++;
-      //   nextQuestion(data);
-      // }
+        i++; // increment question count
+        nextQuestion(data);
+
     }); // form.submit
   };
 
@@ -96,14 +91,22 @@ $(document).ready(function() {
     if (i < data.length) {
       setTimeout(function() {
         displayQuestion(data);
-        console.log("next question displayed");
       }, 1000);
-    // if last question in the quiz, quiz complete
+    // quiz complete once reached last question 
     } else if (i === data.length) {
       setTimeout(function() {
-        $(".main-body").empty();
-        $(".main-body").html("You completed the quiz!");
-        i = 0; // reset question counter
+        var score = Math.floor((k / data.length) * 100);
+        console.log(score + "%");
+
+        // $.post("link/to/post", function(datatopost) {
+
+        // });
+
+        $(".quiz-info").empty();
+        $(".quiz-info").html("You completed the quiz!<br><br> Score: " + score + "%");
+        i = 0; // reset counters
+        j = 0;
+        k = 0; 
       }, 1000);
     }
 
@@ -111,16 +114,54 @@ $(document).ready(function() {
 
   $(".all-quizzes").on("click", "a", function() {
 
+    $(".all-quizzes").show(1000);
     $("a").removeClass("active");
     $(this).addClass("active");
     quizId = $(this).attr("quiz-id");
-    console.log("quiz ID " + quizId);
 
     $.get("/quizzes/" + quizId + "/questions", function(data) {
       displayQuestion(data);
     }); // .get
   }); // on("click", "a")
 
+  $(".take-quiz").click(function() {
+    $(".all-quizzes").show(1000);
+    $("a").removeClass("active");
+    $(".about-project").show();
+    $(".quiz-info").empty();
+    $(".create-quiz-div").hide();
+  });
+
+
+  $(".create-quiz").click(function() {
+    $(".all-quizzes").hide(1000);
+    $(".about-project").hide();
+    $(".quiz-info").empty();
+    $(".create-quiz-div").show();
+
+    $("input[name='question-type']").change(function(){
+      if ($(this).val() === 'multiple') {
+        $(".tf-answer-choices").hide();
+        $(".fill-answer-choice").hide();
+        var mc = $(".mc-answer-choices").show();
+        // .append(mc);
+
+        // $(".q1").append(mc);
+      } else if ($(this).val() === 'boolean') {
+        $(".mc-answer-choices").hide();
+        $(".fill-answer-choice").hide();
+        var tf = $(".tf-answer-choices").show();
+        // $(".q1").append(tf);
+      } else if ($(this).val() === 'fill') {
+        $(".tf-answer-choices").hide();
+        $(".mc-answer-choices").hide();
+        var fill = $(".fill-answer-choice").show();
+        // $(".q1").append(fill);
+      }
+
+    });
+
+  });
 
 
 });
